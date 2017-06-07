0c03ede68f808dab61044895466b8210aef46969
Omar Zorob <omar.zorob@yahoo.com>
2017-06-07 14:34:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/omarz/tipapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.omarz.tipapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText billTotal, tipPercentage;
+    TextView totalTip;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button tipCalc = (Button)findViewById(R.id.tipCalc);
+        billTotal = (EditText)findViewById(R.id.billTotal);
+        tipPercentage = (EditText)findViewById(R.id.tipPercentage);
+        totalTip = (TextView)findViewById(R.id.totalTip);
+        tipCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double totalCost = Double.parseDouble(billTotal.getText().toString());
+                double tipPerc = Double.parseDouble(tipPercentage.getText().toString());
+
+                double tip = (tipPerc / 100) * totalCost;
+                totalTip.setText(Double.toString(tip));
+            }
+        });
+    }
+}

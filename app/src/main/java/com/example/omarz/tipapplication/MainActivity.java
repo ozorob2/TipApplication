16251dc2a4b33979f89177f346c033d632322a06
Omar Zorob <omar.zorob@yahoo.com>
2017-06-12 09:34:56 -0500

    Change Log -- Changed End Value To Be Total Cost For Entire Meal Not Just Tip -- Changed Colors of Background and Button -- App can no longer crash if no values are entered -- No divide by zero error

--- app/src/main/java/com/example/omarz/tipapplication/MainActivity.java
+++ app/src/main/java/com/example/omarz/tipapplication/MainActivity.java
@@ -1,18 +1,19 @@
 package com.example.omarz.tipapplication;
 
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText billTotal, tipPercentage, people;
     TextView totalTip;
 
-    //No Way
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,13 +27,25 @@ public class MainActivity extends AppCompatActivity {
         tipCalc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Double totalCost = Double.parseDouble(billTotal.getText().toString());
-                Double tipPerc = Double.parseDouble(tipPercentage.getText().toString());
-                int numberOfPeople = Integer.parseInt(people.getText().toString());
 
-                Double tip = ((tipPerc / 100) * totalCost)/numberOfPeople;
+                if(billTotal.getText().toString().matches("") || tipPercentage.getText().toString().matches("") || people.getText().toString().matches(""))
+                    Toast.makeText(MainActivity.this, "None Of The Fields Can Be Empty!!", Toast.LENGTH_SHORT).show();
+                else
+                {
+                    double totalCost = Double.parseDouble(billTotal.getText().toString());
+                    double tipPerc = Double.parseDouble(tipPercentage.getText().toString());
+                    int numberOfPeople = Integer.parseInt(people.getText().toString());
 
-                totalTip.setText(Double.toString(tip));
+                    if (numberOfPeople <= 0)
+                        Toast.makeText(MainActivity.this, "Number of People Can Not Be 0!!", Toast.LENGTH_SHORT).show();
+
+                    else
+                    {
+                        double tip = (tipPerc / 100) * totalCost;
+                        double totCost = (totalCost + tip) / numberOfPeople;
+                        totalTip.setText(Double.toString(totCost));
+                    }
+                }
             }
         });
     }
